a46355ef9688195a5b051366c94adec633bd0e55
UGUIS8020 <shibuyamasahiko@gmail.com>
2024-12-01 14:08:50 +0900

    新越谷

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def load_user(user_id):
 
 class RegistrationForm(FlaskForm):
     organization = SelectField('所属', choices=[('uguis', '鶯'),('other', 'その他')], default='uguis', validators=[DataRequired(message='所属を選択してください')])
-    display_name = StringField('表示ネーム LINE名など', validators=[DataRequired(message='表示名を入力してください'), Length(min=3, max=30, message='表示名は3文字以上30文字以下で入力してください')])
+    display_name = StringField('表示名 LINE名など', validators=[DataRequired(message='表示名を入力してください'), Length(min=3, max=30, message='表示名は3文字以上30文字以下で入力してください')])
     user_name = StringField('ユーザー名', validators=[DataRequired()])
     furigana = StringField('フリガナ', validators=[DataRequired()])
     phone = StringField('電話番号', validators=[DataRequired(), Length(min=10, max=15, message='正しい電話番号を入力してください')])
@@ -222,7 +222,7 @@ class RegistrationForm(FlaskForm):
         
 class UpdateUserForm(FlaskForm):
     organization = SelectField('所属', choices=[('uguis', '鶯'), ('other', 'その他')], validators=[DataRequired(message='所属を選択してください')])    
-    display_name = StringField('表示ネーム LINE名など', validators=[DataRequired(), Length(min=3, max=30)])    
+    display_name = StringField('表示名 LINE名など', validators=[DataRequired(), Length(min=3, max=30)])    
     user_name = StringField('ユーザー名', validators=[DataRequired()])    
     furigana = StringField('フリガナ',  validators=[DataRequired()])    
     phone = StringField('電話番号', validators=[DataRequired(), Length(min=10, max=15, message='正しい電話番号を入力してください')])    
